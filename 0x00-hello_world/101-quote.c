bfe51f36a8bbcc4a1807e2b57a7ba1b738b761c4
JoshuaOloton <olotonjoshua@gmail.com>
2022-03-11 07:11:42 +0000

    Corrected error in 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,8 @@
 /* Betty coding style */
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, 
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	return (0);
+	return (1);
 }
