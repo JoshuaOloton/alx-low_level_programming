2bd4bd5de3a1add23787f72a14a73ae671ca63b1
JoshuaOloton <olotonjoshua@gmail.com>
2022-03-11 05:58:19 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* Betty coding style */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return 0;
+}
+
