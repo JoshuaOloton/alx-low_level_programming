ed30d056f77876f38f8fa908dcbfe66100d408ed
JoshuaOloton <olotonjoshua@gmail.com>
2022-03-12 05:04:51 +0000

    Fixed Task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,23 +11,22 @@
 int main(void)
 {
 	int tens;
-  	int ones;
+	int ones;
 
-  	for (tens = 0; tens <= 9; tens++)
-    	{
-      		for (ones = tens + 1; ones <= 9; ones++)
-		{	
-	  		putchar(tens + '0');
-	  		putchar(ones + '0');
-
-	  		if (tens < 8)
-	    		{
-	      			putchar(',');
-	      			putchar(' ');
-	    		}
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-    	}
-  	putchar(10);
+	}
+	putchar(10);
 
 	return (0);
 }
