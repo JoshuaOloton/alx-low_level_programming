17f1d8251d107059041a3a2ecb1c796a68f61289
JoshuaOloton <olotonjoshua@gmail.com>
2022-03-12 18:00:31 +0000

    Fixed Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,14 +14,10 @@ int main(void)
 	int ch = 0;
 	int l = 0;
 
-	/* 00 00 = ij kl*/
-	/* ij kl */
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			/* at start of j loop, set change variable ch to 1 */
-			/* this ch=1 is used to set the l to j+1  */
 			ch = 1;
 			for (k = i; k < 10; k++)
 			{
@@ -32,7 +28,6 @@ int main(void)
 					l = j + 1;
 					ch = 0;
 				}
-				/* if ch=0 ie in between j iteration, l starts iteration at 0 not j+1*/
 				while (l < 10)
 				{
 					putchar(i + '0');
@@ -42,6 +37,7 @@ int main(void)
 					putchar(l + '0');
 					if (i == 9 && j == 8 && k == 9 && l == 9)
 					{
+						putchar('\n');
 						break;
 					}
 					putchar(',');
