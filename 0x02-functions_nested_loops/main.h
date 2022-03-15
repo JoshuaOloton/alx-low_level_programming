2a1c05b177e3e9d7963dc8e3185b16351a6e169b
JoshuaOloton <olotonjoshua@gmail.com>
2022-03-15 22:20:30 +0000

    2. 10 x alphabets

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <unistd.h>
-
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appopriately.
+*/
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void);
 #endif
