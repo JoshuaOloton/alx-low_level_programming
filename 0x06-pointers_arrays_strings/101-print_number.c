7b452d79508f47a04a1ed05011d935526b36c02c
JoshuaOloton <olotonjoshua@gmail.com>
2022-03-26 23:39:18 +0000

    refixed task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,37 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
-* print_number - prints # using _putchar function
-* @n: the integer to print
-*/
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
 void print_number(int n)
 {
-	int ones, size, copy;
+	int m, copy, size;
 
 	size = 1;
+
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -1 * n;
+		_putchar(45);
+		m = n * -1;
 	}
-	ones = n % 10;
-	copy = n / 10;
-	n /= 10;
-	while (copy >= 10)
+	else
+	{
+		m = n;
+	}
+	copy = m;
+
+	while (copy > 9)
 	{
 		copy /= 10;
 		size *= 10;
 	}
-	if (copy > 0)
+
+	while (size >= 1)
 	{
-		while (size >= 1)
-		{
-			_putchar((n / size) + '0');
-			n %= size;
-			size /= 10;
-		}
+		_putchar(((m / size) % 10) + 48);
+		size /= 10;
 	}
-	_putchar(ones + '0');
 }
+
