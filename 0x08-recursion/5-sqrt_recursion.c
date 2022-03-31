e7cfb4d01ba453b56e8f39d311bc878b50a99840
JoshuaOloton <olotonjoshua@gmail.com>
2022-03-31 18:05:19 +0000

    revised task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,25 +3,25 @@
 #include "main.h"
 /**
  * binary - computes square root recursively doing binary search
- * @start: inital number
- * @end: last number within the limit of number
- * @m: given number
- * Return: 1 if not found sqrroot, else sqrroot
+ * @start: lower bound
+ * @end: upper bound
+ * @n: given number
+ * Return: -1 if not found sqrroot, else sqrroot
  **/
-int binary(int start, int end, int m)
+int binary(int start, int end, int n)
 {
-	long mid;
+	int mid, sqr;
 
-	if (end >= start)
+	while (start <= end)
 	{
-		mid = start + (end - start) / 2;
-		if (mid * mid == m)
+		mid = (start + end) / 2;
+		sqr = mid * mid;
+		if (sqr == n)
 			return (mid);
-/* following binary search */
-		if (mid * mid > m)
-			return (binary(start, mid - 1, m));
-		if (mid * mid < m)
-			return (binary(mid + 1, end, m));
+		if (sqr > n)
+			return (binary(start, mid - 1, n));
+		if (sqr < n)
+			return (binary(mid + 1, end, n));
 	}
 	return (-1);
 }
@@ -30,11 +30,11 @@ int binary(int start, int end, int m)
  * @n: given number
  * Return: square root of n or -1
  **/
-	int _sqrt_recursion(int n)
+int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	return (binary(2, n, n));
+	if (n < 0)
+		return (-1);
+	return (binary(0, n, n));
 }
