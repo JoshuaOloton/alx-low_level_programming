6bbea32c01af0f52f3cc63bc506bea7b61c01847
JoshuaOloton <olotonjoshua@gmail.com>
2022-04-02 21:32:49 +0000

    fixed task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-int to_integer(char *s);
+
 /**
  * main - check the code
  * @argc: count of the arguments
@@ -18,31 +17,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	a = to_integer(argv[1]);
-	b = to_integer(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 	printf("%d\n", a * b);
 	return (0);
 }
-
-/**
- * to_integer - convert to integer
- * @s: string
- * Return: int
- */
-int to_integer(char *s)
-{
-	int len, i, a, size;
-
-	for (len = 0; s[len] != 0; len++)
-	{
-	}
-	size = pow(10, len - 1);
-	a = 0;
-	for (i = 0; i < len; i++)
-	{
-		a += ((s[i] - 48) * size);
-		size /= 10;
-	}
-	return (a);
-}
-
