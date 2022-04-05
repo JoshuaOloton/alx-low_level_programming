fae3536edad2019880523d5200fedde560cd1661
JoshuaOloton <olotonjoshua@gmail.com>
2022-04-05 19:23:03 +0000

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,24 +3,37 @@
 #include "main.h"
 
 /**
- * *_strdup - returns a pointer to a newly allocated space with copy of arg
- * @str: string to copy
- * Return: pointer to array or null
+ * *str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to new space in memory or null
  **/
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
 	char *s;
-	int len, i;
+	int l1, l2, i, j;
 
-	for (len = 0; str[len] != '\0'; len++)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (l1 = 0; s1[l1] != '\0'; l1++)
 	{
 	}
-	s = malloc(sizeof(*str) * len);
-	if (s == NULL || str == NULL)
-		return (NULL);
-	for (i = 0; i < len; i++)
+	for (l2 = 0; s2[l2] != '\0'; l2++)
 	{
-		s[i] = str[i];
+	}
+	s = malloc(sizeof(*s1) * (l1 + l2) + 1);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < l1; i++)
+	{
+		s[i] = s1[i];
+	}
+	for (j = 0; j < l2; j++)
+	{
+		s[i] = s2[j];
+		i++;
 	}
 	return (s);
 }
