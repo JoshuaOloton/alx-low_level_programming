a4446c95189d1442b1347d3e661733469d1fa3d1
JoshuaOloton <olotonjoshua@gmail.com>
2022-04-05 00:58:42 +0000

    fixed task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,9 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
-	if (size == 0)
-		return (NULL);
 	s = malloc(sizeof(c) * size);
+	if (size == 0 || s == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		s[i] = c;
