e32363fa17fac3bd5db212fe1d09a0c566577e4e
JoshuaOloton <olotonjoshua@gmail.com>
2022-04-06 22:04:55 +0000

    task 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,9 +11,34 @@
  **/
 char *argstostr(int ac, char **av)
 {
-	int i;
+	char *s;
+	int i, j, k, len, size;
 
-	for (i = 0; i < height; i++)
-		free(grid[i]);
-	free(grid);
+	size = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (len = 0; av[i][len] != '\0'; len++)
+		{
+		}
+		size += len;
+	}
+	s = malloc(sizeof(char) * (size + ac));
+	if (s == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			s[k] = av[i][j];
+			k++;
+			j++;
+		}
+		s[k] = '\n';
+		k++;
+	}
+	return (s);
 }
