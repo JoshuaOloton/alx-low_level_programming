a5e00c46f01010f8adc6f6bec86826e03d4e1d6c
JoshuaOloton <olotonjoshua@gmail.com>
2022-04-07 18:06:54 +0000

    adjusted memory allocation for task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (n >= l2)
 		n = l2;
-	s = malloc(sizeof(*s1) * (l1 + l2) + 1);
+	s = malloc(sizeof(*s1) * (l1 + n) + 1);
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < l1; i++)
