3528e683c2115e28383fef018346be1f06b576be
JoshuaOloton <olotonjoshua@gmail.com>
2022-04-10 02:40:29 +0000

    fixed task 100 - VII

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,9 +17,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	char *p;
 
-	p = malloc(new_size);
-	if (p == NULL)
-		return (NULL);
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
@@ -29,8 +26,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		return (p);
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
 	}
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
