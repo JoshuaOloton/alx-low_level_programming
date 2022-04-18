b7872daf291c80e9e495fd69d466f22e4a2065c8
JoshuaOloton <olotonjoshua@gmail.com>
2022-04-18 02:28:25 +0000

    fixed task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,13 +16,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t *));
 	if (dog == NULL || name == NULL || owner == NULL)
 		return  (NULL);
-	for (i = 0; name[nlen]; nlen++);
-	for (j = 0; name[olen]; olen++);
-	dog->name = malloc(i * sizeof(char *));
-	dog->owner = malloc(j * sizeof(char *));
-	for (i = 0; name[i]; i++)
-	dog->name = name;
+	for (nlen = 0; name[nlen]; nlen++);
+	for (olen = 0; name[olen]; olen++);
+	dog->name = malloc(nlen * sizeof(char *) + 1);
+	dog->owner = malloc(olen * sizeof(char *) + 1);
+	if (dog->name == NULL || dog->owner == NULL)
+		return  (NULL);
+	for (i = 0; i < nlen; i++)
+	{
+		dog->name[i] = name[i];
+	}
+	for (j = 0; j < olen; j++)
+	{
+		dog->owner[j] = owner[j];
+	}
+	dog->name[i] = '\0';
+	dog->owner[j] = '\0';
 	dog->age = age;
-	dog->owner = owner;
+
 	return (dog);
 }
