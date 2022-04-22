314d3016e16a188f3fbe42469293537554e9735b
JoshuaOloton <olotonjoshua@gmail.com>
2022-04-22 05:05:56 +0000

    2.Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,7 +8,7 @@
  * @s: string
  * Return: length of the string
  */
-int _strlen(char *s)
+int _strlen(const char *s)
 {
 	int i;
 
@@ -27,23 +27,26 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	char *newstr;
+	int len, i;
 
 	if (str == NULL || head == NULL)
 		return (NULL);
+	len =  _strlen(str);
 	newstr = malloc(sizeof(char) * len + 1);
 	if (newstr == NULL)
 		return (NULL);
-	for (i = 0; str[i]; i++)
+	for (i = 0; i < len; i++)
 		newstr[i] = str[i];
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
-		free(content);
+		free(newstr);
 		return (NULL);
 	}
-	new->str = content;
+	new->str = newstr;
 	new->len = len;
 	new->next = *head;
 	*head = new;
+
 	return (new);
 }
