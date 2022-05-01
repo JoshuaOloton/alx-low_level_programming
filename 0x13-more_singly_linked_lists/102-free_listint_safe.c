6ac49d64c071aa7f882767fb962e63765a1366db
JoshuaOloton <olotonjoshua@gmail.com>
2022-05-01 01:44:38 +0000

    added function documentation

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -26,6 +26,12 @@ void free_listp2(listp_t **head)
 	}
 }
 
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
+ */
 size_t free_listint_safe(listint_t **h)
 {
 	size_t n = 0;
