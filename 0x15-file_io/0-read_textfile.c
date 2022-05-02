c791c18d9d4ef05801cf08736778386344e09f90
JoshuaOloton <olotonjoshua@gmail.com>
2022-05-02 18:16:54 +0000

    fixed betty documentation error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,11 +5,18 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buf;
-	ssize_t rtwrite, rtread;	
+	ssize_t rtwrite, rtread;
 
 	if (filename == NULL)
 		return (0);
