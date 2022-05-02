619eebd724613da0b0363962a18ff751141c8c69
JoshuaOloton <olotonjoshua@gmail.com>
2022-05-02 22:02:39 +0000

    1.Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+	ssize_t rtwrite;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+	for (len = 0; text_content[len]; len++)
+		;	
+	rtwrite = write(fd, text_content, len);
+	if (rtwrite == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
