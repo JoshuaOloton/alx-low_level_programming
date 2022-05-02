975ed0dd6e18e4e09efe12f8fd8f180967169823
JoshuaOloton <olotonjoshua@gmail.com>
2022-05-02 22:39:24 +0000

    2. Speak gently, she can hear

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * create_file - creates a file
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
+ *
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, len;
+	ssize_t rtwrite;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+	for (len = 0; text_content[len]; len++)
+		;
+	rtwrite = write(fd, text_content, len);
+	if (rtwrite == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
