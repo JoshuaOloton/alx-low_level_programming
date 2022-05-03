929c52f357490c51d25cb1156a2e6605818f80e9
JoshuaOloton <olotonjoshua@gmail.com>
2022-05-03 05:19:17 +0000

    corrected write flag error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: filename.
@@ -15,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_APPEND);
+	fd = open(filename, 0_WRONLY | O_APPEND, 0666);
 	if (fd == -1)
 		return (-1);
 
