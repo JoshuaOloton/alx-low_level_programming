dd87ec3b930bf9454027aa081c328bac500d2d92
JoshuaOloton <olotonjoshua@gmail.com>
2022-05-05 11:48:47 +0000

    fixed error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-void error(int e, char *filename);
+void error(int e, char *argv[]);
 
 /**
  * main - check the code.
@@ -21,28 +21,25 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	if (argc != 3)
-		error(97, "");
+		error(97, argv);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		error(98, argv[1]);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+		error(98, argv);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	if (file_to == -1)
-		error(99, argv[2]);
+	{
+		close(file_from);
+		error(99, argv);
+	}
 	nread = 1024;
 	while (nread == 1024)
 	{
 		nread =  read(file_from, buf, 1024);
 		if (nread == -1)
-			error(98, argv[1]);
+			error(98, argv);
 		nwrite = write(file_to, buf, nread);
 		if (nwrite == -1)
-			error(99, argv[2]);
-	}
-	close_err = close(file_to);
-	if (close_err == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
+			error(99, argv);
 	}
 	close_err = close(file_from);
 	if (close_err == -1)
@@ -50,30 +47,36 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
+	close_err = close(file_to);
+	if (close_err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
 	return (0);
 }
 
 /**
- * error - checks if files can be opened.
+ * error - check if file can be opened
  * @e: error code.
- * @filename: filename
+ * @argv: filename
  * Return: no return.
  */
-void error(int e, char *filename)
+void error(int e, char *argv[])
 {
 	if (e == 97)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	else if (e == 98)
+	if (e == 98)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	else if (e == 99)
+	if (e == 99)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
 }
