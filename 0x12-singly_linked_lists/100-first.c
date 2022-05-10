cd99751f4d760ea15f2960fa8de6caa1848b1680
JoshuaOloton <olotonjoshua@gmail.com>
2022-05-10 22:38:46 +0000

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stddef.h>
+#include <stdio.h>
+
+void printBefore(void) __attribute__((constructor));
+
+void printBefore(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
