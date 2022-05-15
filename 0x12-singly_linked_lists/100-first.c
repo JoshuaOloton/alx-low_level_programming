6373b004ce6858c4f615715ad20877d999a1d520
JoshuaOloton <olotonjoshua@gmail.com>
2022-05-15 13:28:47 +0000

    corrected task 8

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,11 @@
 
 void printBefore(void) __attribute__((constructor));
 
+/**
+ * printBefore - prints from init by the loader before main
+ */
 void printBefore(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
 }
