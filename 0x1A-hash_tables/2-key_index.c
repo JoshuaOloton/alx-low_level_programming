970c8cd5b2da7adf9fa736368544cd1dc2b9b499
JoshuaOloton <olotonjoshua@gmail.com>
2022-07-02 16:23:32 +0000

    corrected betty error task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,14 +3,15 @@
 /**
  * key_index - gives index of array
  * @key: key
- * @value:  size of the array of the hash table
+ * @size:  size of the array of the hash table
  *
- * Return: the index at which the key/value pair should be stored in the array of the hash table
+ * Return: the index at which the key/value pair
+ * should be stored in the array of the hash table
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	int index;
+	unsigned long int index;
 
 	index = hash_djb2(key) % size;
 	return (index);
